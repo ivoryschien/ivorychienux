d91991743a52290c5fa7b8b9f4171ae8084834c1
ivoryschien <shhchien@ucsc.edu>
2023-04-03 14:45:39 -0700

    Initial commit

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ $('a').click(function(e){
   
   _elem.parent('li').addClass('active');
 });
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(sentence.toLowerCase());
+// Expected output: "the quick brown fox jumps over the lazy dog."
